6b92d08a451f8f0ef10d44c06ead25c36bf2a109
Jwala Kompalli <seethalchandra.96@gmail.com>
2023-04-09 23:30:40 -0500

    Day 1 Part 2: Buttons, Touchable Opcity, Props and Navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("ComponentScreen");
+        }}
+        style={styles.buttonStyle}
+        title="Go to Components Demo"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => {
+          navigation.navigate("ListScreen");
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    alignSelf: "center",
+    paddingTop: 10,
+  },
+  buttonStyle: {
+    paddingTop: 50,
   },
 });
 
