81ab50b7f23c2d24ad810fe5c5cf51c868131205
Jwala Kompalli <seethalchandra.96@gmail.com>
2023-04-12 19:24:30 -0500

    Installed React Native Elements Library and Updated the Component Screen

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import { CheckBox } from "@rneui/themed";
 
 const ComponentScreen = () => {
+  const [check1, setCheck1] = useState(false);
   const name = "My Name is Jwala Kompalli";
   const nameJSX = <Text style={styles.text}>{name}</Text>;
   return (
     <View>
       <Text style={styles.text1}>Getting Started with React Native!</Text>
       {nameJSX}
+      <Text style={styles.text}>This is a checkbox</Text>
+      <CheckBox
+        center
+        title="Click Here"
+        checked={check1}
+        onPress={() => setCheck1(!check1)}
+      />
     </View>
   );
 };
